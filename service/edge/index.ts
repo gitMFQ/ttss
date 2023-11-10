17a727be65243044a666068981d740396b513ba8
gitMFQ <652299482@qq.com>
2023-11-10 16:36:06 +0800

    ts

--- service/edge/index.ts
+++ service/edge/index.ts
@@ -59,7 +59,7 @@ export class Service {
       origin: 'chrome-extension://jdiccldimpdaibmpdkjnbmckianbfold',
       headers: {
         'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.66 Safari/537.36 Edg/103.0.1264.44',
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
       },
     })
     return new Promise((resolve, reject) => {
